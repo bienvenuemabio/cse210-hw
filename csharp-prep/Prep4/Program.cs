e0edac6bb21acb585926488c05ecd7f663d34f01
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-05-18 15:05:00 +0000

    added a code to program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+
+        Console.WriteLine("Hello Prep4 World!");
+        
+        while (userNumber != 0)
+        {
+            Console.WriteLine("Enter a list of number, type 0 when finished");
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse (userResponse);
+
+            if (userNumber!=0)
+            {
+                numbers.Add(userNumber);
+            } 
+        
+        }
+        int sum=0;
+        foreach(int number in numbers)
+        {
+             sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average= sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+          if  (number > max)
+          {
+            max = number;
+            Console.WriteLine("The largest number is: {max}");
+          }
+          
+        }
     }
 }
