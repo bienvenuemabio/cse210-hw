6a3d92f93603d4379c3a7e7f516ccd2498874cbc
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-05-18 14:08:58 +0000

    added code to program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your firsname?");
+        String firstname = Console.ReadLine();
+        Console.WriteLine("What is your firsname?");
+        String lastname = Console.ReadLine();
+        Console.WriteLine($"your name is {firstname}, {lastname}");
+
     }
 }
