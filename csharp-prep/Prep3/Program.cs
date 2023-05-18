0f90338a0dc447401520f9ad989005b69f2a063c
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-05-18 14:58:05 +0000

    added some code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        int guessInput= -1;
+        Random randomGeneretor= new Random();
+        int number= randomGeneretor.Next(1,101);
+        while(guessInput != number)
+        {
+            Console.WriteLine("What is the  magic number? ");
+            Console.WriteLine("What is your guess? ");
+            
+            guessInput = int.Parse(Console.ReadLine());
+            
+            if (guessInput < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if(guessInput > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("You guess it");
+            }
+          
+        }
     }
 }
