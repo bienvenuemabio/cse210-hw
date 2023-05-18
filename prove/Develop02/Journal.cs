0c9cf018a749b42ed2df077ef6d26b829283ca28
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-05-18 15:53:04 +0000

    created a journal program

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,12 @@
+public class journal
+{
+    public string _journalTitle="";
+
+    public string _prompt1 = "";
+    public string _prompt2 = "";
+    public string _prompt3 = "";
+    public string _prompt4 = "";
+
+    public string _prompt5 = "";
+    
+}
