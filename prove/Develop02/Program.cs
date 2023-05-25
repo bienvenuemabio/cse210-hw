aa16c1115d862575cb269b5eb306620188e8e20f
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-05-25 02:04:11 +0000

    added code to program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,23 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
-         Console.WriteLine("Hello Develop02 World!");
-        Console.WriteLine("Welcome to our journal program!");
-        Console.WriteLine("Please select one of the following choices"); 
-        string userInput= Console.ReadLine();
-        int _choice= int.Parse(userInput);
-        while (_choice != 0){
-          Console.WriteLine("Please select one of the following choices");
-          
-          Console.WriteLine("1.Write");  
-          Console.WriteLine("2.Display");
-          Console.WriteLine("3.load");
-          Console.WriteLine("4.Save");
-          Console.WriteLine("5.quit");
-          Console.WriteLine("What could you like to do");
-          int _Choice=int.Parse(Console.ReadLine());
+      Console.Write("Hello Develop02 World!");
+      Console.Write("Welcome to our journal program!");
+      Console.Write("1.Write");  
+      Console.Write("2.Display");
+      Console.Write("3.load");
+      Console.Write("4.Save");
+      Console.Write("5.quit");
+      Console.WriteLine("What could you like to do?: "); 
+      string userInput= Console.ReadLine();
+      int _userOption= int.Parse(userInput);
+        
+      while (_userOption != 5)  
+      {
+        if (_userOption ==1)
+        {
+          DateTime theCurrentTime = DateTime.Now;
+          string _dateText = theCurrentTime.ToShortDateString();
+          Console.WriteLine($"{_dateText}: ");
+
+          List <string> prompts = new List<string>();
+          prompts.Add("Who was the most interesting person I interacted with today?");
+          prompts.Add("What was the best part of my day?");
+          prompts.Add("How did I see the hand of the Lord in my life today?");
+          prompts.Add("What was the strongest emotion I felt today?");
+          prompts.Add("If I had one thing I could do over today, what would it be?");
+          prompts.Add("How was the day, good or not?");
+
+          foreach (string p in prompts)
+          {
+            Console.WriteLine(p);
+          }
+
+
+
         }
+      }
+          
+      
     
     }
 }
