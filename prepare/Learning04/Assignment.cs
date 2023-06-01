e8fe523423bd14b8fc2b532f33991cd2c4b1f769
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-06-01 04:32:28 +0000

    created the assignment class

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,16 @@
+public class Assignment {
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string _studentName, string _topic)
+    {
+        _topic = topic;
+
+    }
+
+    public string GetSummary(){
+        return _studentName;
+
+    }
+
+}
