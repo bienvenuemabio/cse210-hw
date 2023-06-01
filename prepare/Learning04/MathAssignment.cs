ee1c07ec238137bbbad9e7148b5fe967eb849819
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-06-01 04:33:04 +0000

    created the mathassignment class

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,8 @@
+public class MathAssignment{
+    private string _textBookSection;
+    private string _problem;
+
+    public string MathAssignment(string _textBookSection, string _problem): base(studentName){
+        _problem = problem;
+    }
+}
