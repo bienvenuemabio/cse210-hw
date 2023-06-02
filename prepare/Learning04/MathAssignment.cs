a18c1e983f8dfbdb855f98638c27bfccb751a3eb
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-06-02 03:32:34 +0000

    added code to the file

--- prepare/Learning04/MathAssignment.cs
+++ prepare/Learning04/MathAssignment.cs
@@ -1,12 +1,13 @@
 public class MathAssignment{
     private string _textBookSection;
-    private string _problem;
+    private string _problems;
 
-    public string MathAssignment(string textBookSection, string problem): base(studentName){
-        _problem = problem;
+    public string MathAssignment( string studentName, string topic, string textBookSection, string problems): base(studentName, topic){
+        _textBookSection= textBookSection;
+        _problems = problems;
     }
 
     public string GetHomeWorkList(string _textBookSection, string problem){
-        return _problem;
+        return $"section{_textBookSection}, problems {_problems}";
     }
 }
