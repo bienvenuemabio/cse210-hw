4813bdf40b42d9cf6e6c6c7ee023f8b324ea1f29
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-06-02 03:33:01 +0000

    added code to the main program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+        Assignment a1= new Assignment("Samuel Benett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        Assignment a2 = new Assignment("Roberto Rodriguez","fraction","7.3","8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeWorkList());
+
+        Assignment a3 = new Assignment("Mary Waters","European History", "the causes of world war II by Mary Waters ");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
