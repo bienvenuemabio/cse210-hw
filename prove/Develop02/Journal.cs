0a7e3b4be47bd41eef363f720813ebdbe2e0e24a
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-06-02 03:39:34 +0000

    added some code to it

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -2,6 +2,11 @@
 public class Journal{
     public string _journalTitle;
     public string _journalWord;
+
+    public Journal()
+    {
+
+    }
     public void Display()
     {
         Console.WriteLine($"{_journalTitle} : {_journalWord}");
