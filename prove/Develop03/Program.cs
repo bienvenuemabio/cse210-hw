0096fb27ec920cdbd25b3fcd1241147faf5e6db4
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-06-10 00:44:37 +0000

    added some change

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,20 +7,21 @@ class Program
         Console.WriteLine("Hello Develop03 World!");
         Reference r= new Reference("matthew",7,7);
         Console.WriteLine(r.GetReferenceString());
-        Word w= new Word("Demandez et l on vous donnera ; cherchez et vous trouverez ; frappez et l on vous ouvrira.");
-        Console.WriteLine(w.GetWord());
+
 
         Console.WriteLine("Please type enter or quit :");
-        Console.ReadLine();
+        string userInput= Console.ReadLine();
     
 
-        if (userInput="enter"){
+        if (userInput=="enter")
+        {
             Console.Clear();
-            else
+        }
+        else
             {
                 
             }
-        }
+        
         
     
     
