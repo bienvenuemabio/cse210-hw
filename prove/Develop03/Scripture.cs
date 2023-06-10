df9417bd3d12b0587f1685c55e65f6bee0b4d779
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-06-10 00:47:15 +0000

    added some cod

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,6 +1,11 @@
 public class Scripture{
     private List<string> _texts = new List<string>();
 
+    public Scripture()
+    {
+        
+    }
+
 
 
     public void displayScripture(string text){
