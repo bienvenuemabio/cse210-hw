06e2fa5c9810b12d1f13f7f304ade4df9831a90c
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-06-10 00:45:41 +0000

    added some change

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,33 +1,34 @@
-using system;
-public class Word
+using System.Collections.Generic;
+public class Words
 {
     private List<string> _words = new List<string>();
 
-    public void SetWord(string words)
+    public Words()
     {
-        _words= words;
+        _words.Add("Demander et l'on");
+        _words.Add("vous donnera,");
+        _words.Add("chercher et vous trouverez;");
+        _words.Add("frappez et");
+        _words.Add("et l'on vous ouvrira");
+
+    }   
+    public void hide()
+    {
 
     }
 
-    public string GetWord(){
-        return _words;
-    }
-     
-    
-
-    public void hide(){
+    public void Show()
+    {
 
     }
 
-    public void Show(){
-
-    }
-
-    public void IsHidden(){
+    public void IsHidden()
+    {
 
     }
 
     public void GetRenderedText(){
+    
 
     }
 }
