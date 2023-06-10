cc3cc52d2ca8f5e224def7faa11733c5566e6b31
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-06-10 00:48:06 +0000

    added some change

---
+++ prove/Develop04/Actitity.cs
@@ -0,0 +1,30 @@
+public class Activity
+{
+    private string _name;
+    private string _description;
+    private int _duration;
+
+    public Activity(string name, string description, int duration)
+    {
+        _name = name;
+        _description= description;
+        _duration= duration;
+
+
+    }
+
+    public string GetActivityName()
+    {
+        return _name;
+    }
+
+    public string GetActivityDescription()
+    {
+        return _description;
+    }
+
+    public int getActivityduration()
+    {
+        return _duration;
+    }
+}
