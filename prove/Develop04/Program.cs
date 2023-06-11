1d7591b927de786d040ab96e4994b183f5a2d31c
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-06-11 05:11:12 +0000

    added code to the main program

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -8,10 +8,37 @@ class Program
         do
         {
             Console.WriteLine("Menu Option:");
-            Console.WriteLine("1.Breathing Activity");  
-            Console.WriteLine("2.Reflecting Activity");
-            Console.WriteLine("3.Listing Activity");
+            Console.WriteLine("1.Start Breathing Activity");  
+            Console.WriteLine("2.Start Reflecting Activity");
+            Console.WriteLine("3.Start Listing Activity");
             Console.WriteLine("4.Quit");
+            Console.WriteLine("Select your choice:");
+
+            int _userChoice= int.Parse(Console.ReadLine());
+
+            if (_userChoice == 4)
+            {
+                quit = true;
+            }
+            else
+            {
+                quit= false;
+            }
+
+            switch (_userChoice)
+            {
+                case 1: 
+
+                    break;
+                
+                case 2:
+
+                    break;
+                
+                case 3: 
+
+                    break;
+            }
         
         
 
