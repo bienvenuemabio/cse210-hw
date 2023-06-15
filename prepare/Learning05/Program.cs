f71814cfcb4b64c0254269d36bc5bcfb7422297e
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-06-15 03:32:57 +0000

    added some code to

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        Square s1= new square();
+        s1.SetSide(2);
+        console.WriteLine(s1.GetArea());
+
+
     }
 }
