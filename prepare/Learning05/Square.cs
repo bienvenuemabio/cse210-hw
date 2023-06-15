d4aa2704288ea606a6fe3236fbe409cc5fb771c5
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-06-15 03:16:04 +0000

    creted a class square

---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,24 @@
+public class Square: Shape
+{
+    private double _side;
+    public Square(string color, double _side): base(color)
+    {
+        _side= side;
+
+    }
+
+    public double SetSide()
+    {
+        _side= side;
+
+    }
+    public void GetSide()
+    {
+        return _side;
+    }
+    public override double GetArea()
+    {
+        return _side* _side;
+
+    }
+}
