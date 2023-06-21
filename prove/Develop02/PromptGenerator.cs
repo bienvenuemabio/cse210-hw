67c4c5edf26bb19683ae5f6c9a1d961624696cbc
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-06-21 00:14:41 +0000

    created a class generator

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -2,12 +2,12 @@ public class PromptGenerator
 {
     public string _prompt;
 
-}
-public PromptGenerator
+public PromptGenerator()
 {
-
 }
 public void DisplayGeneratePrompt()
 {
     Console.WriteLine(_prompt);
+}
+
 }
