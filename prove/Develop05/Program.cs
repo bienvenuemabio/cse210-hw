41ec48d09feee24684bdb22f5a233ec6c3384e1e
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-06-27 00:50:39 +0000

    added more code

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,19 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        int points = 0;
+
+        //int points = 0;
 
         bool finished = false;
         do
         {
-            Console.WriteLine($"You have {points} points");
-            Console.WriteLine("Menu Options:");
-            Console.WriteLine("1.Create new goal");
-            Console.WriteLine("2.List Goals");
-            Console.WriteLine("3.Save Goals");
-            Console.WriteLine("4.Load Goals");
-            Console.WriteLine("5.Record Event");
-            Console.WriteLine("6.Quit");
+            //Console.WriteLine($"You have {points} points");
+            DisplayMenu();
             Console.Write("Select a choice from the menu: ");
             int userInput= int.Parse(Console.ReadLine());
 
@@ -30,42 +25,84 @@ class Program
                 Console.Write("Which type of goal would you like to create? ");
                 int response = int.Parse(Console.ReadLine());
 
-                List<Goals> goal = new List<Goals>();
-                goal.Add(new Goals());
+                //List<Goals> goal = new List<Goals>();
+                //goal.Add(new Goals());
+                
 
                 if (response== 1)
                 {
-                    goal.Add(new SimpleGoal());
+                    SimpleGoal simple = new SimpleGoal();
+                    DisplayMenu();
+                    
+                    //goal.Add(new SimpleGoal());
                 }
                 
                 else if (response == 2)
                 {
-                    goal.Add(new EternalGoal());
+                    EternalGoal eternal = new EternalGoal();
+                    DisplayMenu();
+                
+                    //goal.Add(new EternalGoal());
 
                 }
                 else if (response== 3)
                 {
-                    goal.Add(new CheckListGoal());
+                    CheckListGoal check= new CheckListGoal();
+                    //goal.Add(new CheckListGoal());
+                    DisplayMenu();
 
                 }
 
-                foreach (Goals goals in goal)
-                {
-                    goals.GetName();
-                    goals.GetDescription();
-                    goals.GetPoint();
-
-                }
+                
                 
             }
             else if (userInput==2)
             {
                 finished = false;
+                SimpleGoal simple= new SimpleGoal();
+                simple.GetName();
+                Console.WriteLine("The goals are: ");
+                Console.WriteLine($"[ ] {simple.GetName()}, {simple.GetDescription}");
+
 
             }
+            else if (userInput == 3)
+            {
+                finished= false;
+                Save s = new Save();
+            }
+            else if (userInput == 4)
+            {
+                Load l = new Load();
+                finished= false;
+            }
+            else if (userInput == 5)
+            {
+                finished= false;
+
+            }
+            else
+            {
+                Console.Write("Thank you for playing the game.")
+                finished=true;
+            }
+
 
         } while(finished);
-        
+      static void DisplayMenu()
+        {
+            int points = 0;
+            Console.WriteLine($"You have {points} points");
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1.Create new goal");
+            Console.WriteLine("2.List Goals");
+            Console.WriteLine("3.Save Goals");
+            Console.WriteLine("4.Load Goals");
+            Console.WriteLine("5.Record Event");
+            Console.WriteLine("6.Quit");
+
+
+        }  
 
     }
 }
