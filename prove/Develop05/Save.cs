eb0309d6e7c57caf6881f76f81620aba9ea4ae8b
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-06-27 00:50:55 +0000

    added some code

--- prove/Develop05/Save.cs
+++ prove/Develop05/Save.cs
@@ -2,6 +2,12 @@ public class Save
 {
     private string _filename;
 
+    public Save()
+    { 
+        Console.WriteLine("What is your file name?: ");
+        _filename= Console.ReadLine();
+    }
+
     public string Getfile()
     {
         return _filename;
