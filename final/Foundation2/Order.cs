1f2710a388377c26cdceec78000358bc6c0f5eca
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-07-06 05:56:23 +0000

    created a class Order

---
+++ final/Foundation2/Order.cs
@@ -0,0 +1,20 @@
+public class Order
+{
+    public List<Products> _products= new List<Products>();
+    public List<Customer> _customers = new List<Customer>();
+
+    public Order()
+    {
+
+    }
+
+    public void TotalCostOrdered()
+    {
+
+    }
+
+    public void DisplayPackingLabel()
+    {
+        
+    }
+}
