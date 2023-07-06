9fe6b8d3841e8e031ebc1d116af3e7225db5913d
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-07-06 07:30:19 +0000

    added some code

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -1,5 +1,8 @@
 public class Order
 {
+    private int _shippingCost =0;
+    private int _totalCost =0;
+    private int _totalPrice=0;
     public List<Products> _products= new List<Products>();
     public List<Customer> _customers = new List<Customer>();
 
@@ -10,11 +13,43 @@ public class Order
 
     public void TotalCostOrdered()
     {
+        foreach (Products product in _products)
+        {
+            product.PriceofProduct();
+            _totalCost += _amount;
+            
+        }
+        if () 
+            {
+                _shippingCost = 5;
+                
+            }
+            else 
+            {
+                _shippingCost = 35;
+            }
+    
+        _totalPrice= _totalCost * _shippingCost;
+        Console.WriteLine($"The total Price is: {_totalPrice}");
 
     }
 
     public void DisplayPackingLabel()
     {
-        
+        foreach(Products product in _products)
+        {
+            product.ProductInfo();
+        }
+
+
+    }
+
+    public void DisplayShippingLabel()
+    {
+        foreach (Customer customer in _customers)
+        {
+            customer.DisplayCustomerInfo();
+        }
+
     }
 }
