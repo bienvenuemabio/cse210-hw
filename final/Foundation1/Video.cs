04ddfd433ac6fc64afdf017eed8517433a8dd65a
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-07-09 01:33:44 +0000

    added some code

--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -3,34 +3,47 @@ public class Video
     public string _title;
     public string _author;
     public int _length;
-    public List<Comment> _comments= new List<Comment>();
+    public List<Comment> _comments;
+
+    public int _commentNumber;
 
     public Video(string title, string author, int length)
     {
         _title = title;
         _author= author;
         _length = length;
-        //_comment= new List<string>();
+       _comments= new List<Comment>();
 
     }
-    public void StoreComment()
+    public void StoreComment(Comment comments)
     {
-
+          _comments.Add(comments);
 
     }
-    public void CommentNumber(int length)
+    public void CommentNumber(int CommentNumber)
     {
+        foreach(Comment comment in _comments)
+        {
+            _commentNumber= _comments.Count();
+        }
+        Console.WriteLine($"There are {_commentNumber} comments made");
         
     }
 
     public void DisplayVideo()
     {
-        Console.WriteLine($"{_title}, {_author}, {_length}");
-        Console.WriteLine("comment:");  
+        Console.WriteLine($"Video title:{_title}");
+        Console.WriteLine($"Author:{_author}");
+        Console.WriteLine($"Length:{_length} seconds");
+        Console.WriteLine("The comments of this video are:");  
 
         foreach (Comment comment in _comments)
         {
             comment.DisplayComment();
+            _commentNumber =_comments.Count();
         }
+
+        Console.WriteLine($"There are {_commentNumber} comments made");
+
     }
 }
