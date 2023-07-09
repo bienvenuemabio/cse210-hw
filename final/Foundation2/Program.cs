3a57eca1ea8b1ed35b75031aeec387faa44b5627
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-07-09 01:34:01 +0000

    added some code

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation2 World!");
+        Order order1 = new Order();
+        Products product1 = new Products();
+        product1.SetProductId("prod123");
+        product1.GetProductId();
+        product1.SetName("phone");
+        product1.GetName();
+        product1.SetPrice(1000);
+        product1.GetPrice();
+        product1.SetQuantity(8);
+        product1.GetQuantity();
+        product1.ProductInfo();
     }
 }
