b779f08a1c6563eadd4a43c023b4bbdafded51f5
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-07-11 03:21:15 +0000

    added code

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -1,21 +1,27 @@
 public class Order
 {
-    private int _shippingUsa =5;
-    private int _totalCost =0;
-    private int _totalPrice=0;
-    private int _shippingCost= 35;
+    private int _totalCost;
+    private int _totalPrice;
+    private int _shippingCost;
     public List<Products> _products;
     public List<Customer> _customers;
 
-    public Order()
+    public Order(int totalPrice, int totalCost, int shippingCost)
     {
+        _totalCost=0;
+        _totalPrice=0;
+        _shippingCost=35;
+        _products= new List<Products>();
+        _customers= new List<Customer>();
 
     }
 
-    public void AddProduct(Products products)
+    public void AddProduct(Products product)
     {
-        _products.Add(products);
+        _products.Add(product);
     }
+
+
 
     public void AddCustomer(Customer customers)
     {
@@ -24,13 +30,20 @@ public class Order
 
     public void TotalCostOrdered()
     {
-        foreach (Products product in _products)
+       foreach (Products product in _products)
         {
            _totalCost += product.PriceofProduct();
             
         }
         _totalPrice= _totalCost * _shippingCost;
-        Console.WriteLine($"The total Price is: {_totalPrice}");
+            Console.WriteLine($"The total Price is: {_totalPrice}");
+
+           // _shippingCost =35;
+            //_totalPrice= _totalCost * _shippingCost;
+           // Console.WriteLine($"The total Price is: {_totalPrice}");
+
+        //}
+        
 
     }
 
