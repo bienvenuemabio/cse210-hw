ce5a9c5927b23693fb8b00730d89257a8cbe7d17
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-07-12 00:03:44 +0000

    created a class Event

---
+++ final/Foundation3/Event.cs
@@ -0,0 +1,43 @@
+public class Event
+{
+    private string _title;
+    private string _description;
+    private string _date;
+    private string _time;
+    private string _address;
+
+    public Event(string title, string description,string date, string time, string address)
+    {
+        _title= title;
+        _description= description;
+        _date = date;
+        _time = time;
+        _address = address;
+
+    }
+
+    public void GenerateMsg()
+    {
+
+    }
+
+    public void StandardDetails()
+    {
+
+    }
+
+    public void FullDetails()
+    {
+
+    }
+
+    public void ShortDescription()
+    {
+
+    }
+
+    public void DisplayEventInfo()
+    {
+        
+    }
+}
