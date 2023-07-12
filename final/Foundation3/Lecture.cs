63e9fff13311a498e4fd97c63d1a1cfa6b91b2b9
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-07-12 00:13:35 +0000

    added some change

--- final/Foundation3/Lecture.cs
+++ final/Foundation3/Lecture.cs
@@ -2,4 +2,10 @@ public class Lecture : Event
 {
     private string _speaker;
 
+    public Lecture(string speaker, string title, string description, string date, string time, string address  ): base( title,description,date, time,address)                                                                                                                                                                                                                                                                                              )
+    {
+        _speaker= speaker;
+
+    }
+
 }
