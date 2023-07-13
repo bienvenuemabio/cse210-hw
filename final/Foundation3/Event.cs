c667fa7f60dad613b9ef48421fea96ca541f0fde
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-07-13 01:10:25 +0000

    Added some code to event

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -4,9 +4,9 @@ public class Event
     private string _description;
     private string _date;
     private string _time;
-    private string _address;
+    private Address _address;
 
-    public Event(string title, string description,string date, string time, string address)
+    public Event(string title, string description,string date, string time, Address address)
     {
         _title= title;
         _description= description;
@@ -23,6 +23,13 @@ public class Event
 
     public void StandardDetails()
     {
+        Console.WriteLine("Standard Details");
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"Description: {_description}");
+        Console.WriteLine($"Date: {_date}");
+        Console.WriteLine($"Time: {_time}");
+        Console.WriteLine($"Address: {_address}");
+
 
     }
 
