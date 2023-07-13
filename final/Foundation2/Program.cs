20af333936bab2ad0f936400153e3074a832b12f
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-07-13 23:55:18 +0000

    make the program work

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation2 World!");
-        Order order1 = new Order(0,0,35);
+        Order order1 = new Order(0,0,5,35);
         Products product1 = new Products();
         product1.SetProductId("prod123");
         product1.GetProductId();
@@ -15,13 +15,28 @@ class Program
         product1.GetPrice();
         product1.SetQuantity(8);
         product1.GetQuantity();
+        Products product2= new Products();
+        product1.SetProductId("prod124");
+        product1.GetProductId();
+        product1.SetName("machine");
+        product1.GetName();
+        product1.SetPrice(15000);
+        product1.GetPrice();
+        product1.SetQuantity(2);
+        product1.GetQuantity();
         order1.AddProduct(product1);
+        order1.AddProduct(product2);
+
+
         Address customadress1= new Address("a001","provo","salt lake","usa");
         Customer customer1= new Customer("landry",customadress1);
         order1.AddCustomer(customer1);
         order1.DisplayPackingLabel();
         order1.DisplayShippingLabel();
         order1.TotalCostOrdered();
+        Console.WriteLine();
+
+
         
     }
 }
