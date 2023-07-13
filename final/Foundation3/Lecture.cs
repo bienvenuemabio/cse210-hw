2ca3025621790e9129e7a93ae33e885c4261b565
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-07-13 01:11:21 +0000

    Added some code to Lecture class

--- final/Foundation3/Lecture.cs
+++ final/Foundation3/Lecture.cs
@@ -1,11 +1,23 @@
-public class Lecture : Event
+public class Lecture: Event
 {
-    private string _speaker;
+    private string _speakerName;
+    private int _capacity;
 
-    public Lecture(string speaker, string title, string description, string date, string time, string address  ): base( title,description,date, time,address)                                                                                                                                                                                                                                                                                              )
+    public Lecture(string speakerName, int capacity, string title, string description, string date, string time, Address address  ): base( title,description,date, time,address)                                                                                                                                                                                                  
     {
-        _speaker= speaker;
+        _speakerName = speakerName;
+        _capacity= capacity;
 
+    }
+
+    public string  GetSpeaker()
+    {
+        return _speakerName;
+    }
+
+    public int GetCapacity()
+    {
+        return _capacity;
     }
 
 }
