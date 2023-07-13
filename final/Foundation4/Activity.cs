e1d30c07de0f86f9c19edd2ff61193c43aa30d6f
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-07-13 23:56:27 +0000

    created a activity class

---
+++ final/Foundation4/Activity.cs
@@ -0,0 +1,25 @@
+public class Activity
+{
+    private string _name;
+    private string _distance;
+    private int _speed;
+    private int _pace;
+    private string _date;
+
+    public Activity(string name, string distance, int speed, int pace, string date)
+    {
+        _name = name;
+        _distance = distance;
+        _speed    = speed;
+        _pace= pace;
+        _date= date;
+
+
+    }
+
+    public void Getsummary()
+    {
+
+    }
+
+}
