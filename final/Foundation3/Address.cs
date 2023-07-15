d4f0a55be127f771af2d13d28cdb997acce3f297
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-07-15 11:44:17 +0000

    added code to address

--- final/Foundation3/Address.cs
+++ final/Foundation3/Address.cs
@@ -2,10 +2,17 @@ public class Address
 {
     private string _id;
     private string _city;
+    private string _country;
 
-    public Address(string id, string city)
+    public Address(string id, string city, string country)
     {
         _id=id;
         _city=city;
+        _country= country;
+    }
+
+    public string GetAddressInfo()
+    {
+        return $" Number:{_id} ,{_city},{_country}";
     }
 }
