de732de60cd841ac066fe9cf76180bf41e22ad25
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-07-15 11:44:59 +0000

    added methode to event

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -1,13 +1,16 @@
 public class Event
 {
+    private string _type;
     private string _title;
     private string _description;
     private string _date;
     private string _time;
     private Address _address;
+    private string _addresses;
 
-    public Event(string title, string description,string date, string time, Address address)
+    public Event( string type, string title, string description,string date, string time, Address address)
     {
+        _type= type;
         _title= title;
         _description= description;
         _date = date;
@@ -16,28 +19,36 @@ public class Event
 
     }
 
-    public void GenerateMsg()
-    {
-
-    }
-
     public string StandardDetails()
     {
-        return $"Title: {_title}, Description: {_description},Date: {_date}, Time: {_time},Address: {_address}";
+        return $"Title: {_title} \n Description: {_description} \n Date: {_date} \n Time: {_time} \n Address: {_address.GetAddressInfo()}";
     }
 
-    public void FullDetails()
+    public string FullDetails()
     {
+        return $"Title: {_title} \n Description: {_description} \n Date: {_date} \n Time: {_time} \n Address: {_address.GetAddressInfo()} \n Type: {_type} \n ";   
 
     }
 
-    public void ShortDescription()
+    public string ShortDescription()
     {
+        return $"Type:{_type} \n Date:{_date} \n Time: {_time}";
+
 
     }
 
     public void DisplayEventInfo()
     {
         
+    }
+
+    public Address GetAddress(){
+        return _address;
+    }
+
+    public void GetAddresses()
+    {
+        _addresses= _address.GetAddressInfo();
+        Console.WriteLine(_addresses);
     }
 }
