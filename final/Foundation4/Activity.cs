5d76ee16a05437b8ae45a6cfc45c7ac6dd59a6cb
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-07-16 03:46:26 +0000

    added a list of activity

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -3,34 +3,37 @@ public class Activity
     private string _name;
     private string _date;
     private int _length;
-    private int _distance;
-    private int _speed;
-    private int _pace;
+    private int _distance= 0;
+    private int _speed = 0;
+    private int _pace = 0;
 
     public Activity(string name, string date, int length)
     {
         _name = name;
         _date = date;
         _length = length;
+
     }
 
     public virtual int GetDistance()
     {
-        return _distance;
+        return _distance += 60;
 
     }
     public virtual int GetSpeed()
     {
         return _speed;
-
-
     }
 
     public virtual int GetPace()
     {
         return _pace;
-
     }
+    public void SetDistance(int distance)
+    {
+      _distance= distance;
+    }
+    
     public string Summary()
     {
         return $"{_date} {_name}({_length}min): Distance {_distance}km, Speed: {_speed} kph, Pace: {_pace} min per km";
