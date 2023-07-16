a0b95e729b8f8e8f3f2767ef760188eb4dafd5c6
bienvenuemabio <k.bienvenuefelicite07@gmail.com>
2023-07-16 03:47:02 +0000

    added code to class

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -6,7 +6,24 @@ class Program
     {
         Console.WriteLine("Hello Foundation4 World!");
 
-        Running running1= new Running("juntion",30,"Running", "03 Nov 2022",25);
-        Console.WriteLine(running1.Summary());
+        List<Activity> _activities = new List<Activity>();
+
+        Running activity1= new Running("juntion",30,"Running", "03 Nov 2022",25);
+        _activities.Add(activity1);
+
+        Cycling activity2= new Cycling(90,"Cycling","12 Jun 2013",40);
+        _activities.Add(activity2);
+
+        Swimming activity3 = new Swimming(60,"Swimming","25 Feb 2023",60);
+        _activities.Add(activity3);
+
+        foreach(Activity activity in _activities)
+        {
+            string summary = activity.Summary();
+            int _distance= activity.GetDistance();
+            int _speed = activity.GetSpeed();
+            int _pace = activity.GetPace();
+            Console.WriteLine(summary);
+        }
     }
 }
